73dbf82443ec5088b19fca5d960dd30b21aa0af9
te36 <timothyeng36@gmail.com>
2017-05-02 01:39:05 +0000

    Added getRawButton() for getting a representation of the Joystick's button.

--- Joystick.js
+++ Joystick.js
@@ -1,6 +1,14 @@
 /*
 * This is a class that represents a generalized Joystick.
 */
+
+// Creates a Joystick instance from a default HTML5 Gamepad object.
 function Joystick(_stick) {
     this.stick = _stick;
-}
+};
+
+// Gets a GamepadButton instance that represents the Joystick's button at the specified instance.
+Joystick.prototype.getRawButton = function(index) {
+    return this.stick.buttons[index];
+};
+
