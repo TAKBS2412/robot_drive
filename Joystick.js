4cc67988ede8d5b5603cf2bbb846793acece8ac4
te36 <timothyeng36@gmail.com>
2017-05-10 00:52:10 +0000

    Removed unnecessary semicolon.

--- Joystick.js
+++ Joystick.js
@@ -5,7 +5,7 @@
 // Creates a Joystick instance from a default HTML5 Gamepad object.
 function Joystick(_stick) {
     this.stick = _stick;
-};
+}
 
 // Gets a GamepadButton instance that represents the Joystick's button at the specified index.
 Joystick.prototype.getRawButton = function(index) {
