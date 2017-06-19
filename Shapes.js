7d825b7bf9a60a2e2b60466707a5c3787be110e1
te36 <timothyeng36@gmail.com>
2017-06-19 22:09:58 +0000

    Represent vectors in component form, not with angle and magnitude.

--- Shapes.js
+++ Shapes.js
@@ -11,9 +11,13 @@ function Point(_x, _y) {
 }
 
 /*
- * This class represents a vector in 2D space.
+ * This class represents a vector in 2D space (in component form).
+ * The vector is represented in component form because we don't need angle and magnitude in this particular case (so that will save us a square root and arctangent call).
 */
-function Vector(_direction, _magnitude) {
-    this.direction = _direction;
-    this.magnitude = _magnitude;
+
+// Creates a vector in component form from 2 points. 
+function Vector(_point1, _point2) {
+    this.deltax = _point2.x - _point1.x;
+    this.deltay = _point2.y - _point1.y;
 }
+
